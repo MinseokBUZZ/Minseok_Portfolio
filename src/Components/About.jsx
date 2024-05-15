e0c2c6e5eb601063e1344f1bea5a339590eddd0a
MinseokBUZZ <minseokshim9@gmail.com>
2024-05-14 20:31:59 -0400

    test

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -18,10 +18,10 @@ const About = () => {
       <h2>Get To Know Me</h2>
       <div className="words" style={{ whiteSpace: "pre-line" }}>
         {`I am a dedicated web developer known for my unwavering perseverance in overcoming any challenges that I face when I do coding. 
-With an optimistic outlook, I approach every coding problem as an opportunity to learn and grow. 
-I take my responsibilities seriously, ensuring the reliability and integrity of my work. 
-In addition, I am a quick learner and collaborative team player who promotes a positive environment.
-I enjoy logging and sharing everything I learn.`}
+        With an optimistic outlook, I approach every coding problem as an opportunity to learn and grow. 
+        I take my responsibilities seriously, ensuring the reliability and integrity of my work. 
+        In addition, I am a quick learner and collaborative team player who promotes a positive environment.
+        I enjoy logging and sharing everything I learn.`}
       </div>
     </div>
   )
